484aa6115a473a4a54f1fb0c74a5c07d5ef7723c
DESKTOP-VQBRS1U\tyirl <tyirluf@gmail.com>
2023-07-17 09:58:27 +0100

    no message

--- B.O/pages/processar/proc_add_cli.php
+++ B.O/pages/processar/proc_add_cli.php
@@ -3,14 +3,16 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obter os dados do formulário
     $primeiro_nome = $_POST["prim_nome"];
+    $password = sha1($_POST["pass"]); // Aplicar o SHA1 na senha
     $email = $_POST["email"];
     $anos_experiencia = $_POST["exp"];
     $ultimo_nome = $_POST["ult_nome"];
+    $confirmar_password = sha1($_POST["confir_pass"]); // Aplicar o SHA1 na senha de confirmação
+
     $telefone = $_POST["telefone"];
     $especializacoes = $_POST["especilizacao"];
     $data_nascimento = $_POST["data_nasc"];
     $descricao = $_POST["desc"];
-    $genero = $_POST["genero"];
 
     // Processar a imagem (se enviada)
     if (isset($_FILES["pic"]) && $_FILES["pic"]["error"] == 0) {
@@ -21,29 +23,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $imagem = ""; // Definir um valor padrão caso nenhuma imagem tenha sido enviada
     }
 
-    require("../../php/connect.php");
+    require("../php/connect.php");
 
     // Obter o ID da função com base no nome da função enviado pelo formulário
     $funcaoId = $_POST["funcao"];
 
-    // Consulta SQL para obter a senha predefinida do funcionário com base no ID
-    $sqlSenha = "SELECT senha FROM funcionarios WHERE func_id = ?";
-    $stmt = $conn->prepare($sqlSenha);
-    $stmt->bind_param("s", $id);
-    $stmt->execute();
-    $stmt->bind_result($password);
-    $stmt->fetch();
-    $stmt->close();
-
-    // Aplicar o SHA1 à senha predefinida
-    $password = sha1($password);
-
-    $sql = "INSERT INTO funcionarios (primeiro_nome, senha, email, anos_experiencia, ultimo_nome,  telefone, especializacao, data_nascimento, descricao, imagem, funcao, genero) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO funcionarios (primeiro_nome, senha, email, anos_experiencia, ultimo_nome, confirmar_senha, telefone, especializacao, data_nascimento, descricao, imagem, funcao) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $cad_usuario = $conn->prepare($sql);
-    $cad_usuario->bind_param('ssssssisssss', $primeiro_nome, $password, $email, $anos_experiencia, $ultimo_nome, $telefone, $especializacoes, $data_nascimento, $descricao, $imagem, $funcaoId, $genero);
+    $cad_usuario->bind_param('ssssssisssss', $primeiro_nome, $password, $email, $anos_experiencia, $ultimo_nome, $confirmar_password, $telefone, $especializacoes, $data_nascimento, $descricao, $imagem, $funcaoId);
 
     if ($cad_usuario->execute()) {
-        header("Location: ../../?p=3"); // redirect
+        // header("Location: ../../?p=1"); // redirect
         exit;
     } else {
         echo "Erro ao adicionar o funcionário: " . $cad_usuario->error;
